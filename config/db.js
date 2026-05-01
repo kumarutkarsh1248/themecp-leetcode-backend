ce3e9698f081679dceb6c0d49594d81a5be16e77
Kumar Utkarsh <nvnukumarutkarsh@gmail.com>
2026-05-01 15:56:09 +0530

    debugging

--- config/db.js
+++ config/db.js
@@ -3,6 +3,10 @@ const mysql = require("mysql2/promise");
 let db;
 
 async function connectDB() {
+    console.log("DB_HOST:", process.env.DB_HOST);
+    console.log("DB_PORT:", process.env.DB_PORT);
+    console.log("DB_USER:", process.env.DB_USER);
+    console.log("DB_NAME:", process.env.DB_NAME);
     db = await mysql.createConnection({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
