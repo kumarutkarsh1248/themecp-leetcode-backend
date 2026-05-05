72fc8ae44352f7f8ceafa871ba82d6621031c690
Kumar Utkarsh <nvnukumarutkarsh@gmail.com>
2026-05-05 16:13:44 +0530

    small changes

--- app.js
+++ app.js
@@ -11,19 +11,22 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:3001",
+    "http://localhost:3000",
     "https://themecp-leetcode-frontend.vercel.app"
   ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
-}));app.use(express.json());
+}));
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Backend is working 🚀");
 });
+
 app.use("/users", userRoutes);
 app.use("/problems", problemRoutes);
 app.use("/leetcode", leetcodeRoutes);
 app.use("/contest", contestRoutes);
 
 module.exports = app;
-
